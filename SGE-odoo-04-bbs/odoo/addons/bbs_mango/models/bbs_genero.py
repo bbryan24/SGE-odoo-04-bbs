195f095238a2570b4e488fff87901fb6761fcaa1
bbryan24 <163444203+bbryan24@users.noreply.github.com>
2026-01-19 19:48:51 +0100

    Arreglo

--- SGE-odoo-04-bbs/odoo/addons/bbs_mango/models/bbs_genero.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class Genero(models.Model):
-    _name = 'bbs_mango.bbs_genero'
-    _description = 'Etiqueta o género de manga'
-
-    name = fields.Char(string='Nombre', required=True)
-    mangas_ids = fields.Many2many('bbs_mango.bbs_manga', string='Mangas')
